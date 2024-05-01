7769f03054240678dd00696887f61ff316ea25b4
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2024-05-01 10:21:10 +0100

    major refactor, OrcaCamera class handles comms

--- control/src/orca_quest/controller.py
+++ control/src/orca_quest/controller.py
@@ -1,10 +1,7 @@
 """Class to control camera. Initially, IPC communication as this is how the cameras will be interacted with."""
 
-from odin_data.ipc_channel import IpcChannel
-from odin_data.ipc_message import IpcMessage
 from odin.adapters.parameter_tree import ParameterTree, ParameterTreeError
-
-from functools import partial
+from orca_quest.orca_camera import OrcaCamera
 
 import logging
 
@@ -13,117 +10,34 @@ class OrcaError(Exception):
     pass
 
 class OrcaController():
-    """Class to send commands via IPC to ORCA-Quest camera."""
+    """Class to consolidate ORCA-Quest camera controls"""
 
     def __init__(self, num_cameras, endpoints, names):
         """This constructor initialises the object and builds parameter trees."""
 
+        # Internal variables
         self.cameras = []
-        self.msg_id = 0
-
-        tree = {}
         camtrees = []
+        tree = {}
 
+        # Raise error if insufficient endpoints for cameras
         if len(endpoints) < num_cameras:
             raise OrcaError("Fewer endpoints defined than number of cameras in config.")
 
         for i in range(num_cameras):
-            camera = IpcChannel(IpcChannel.CHANNEL_TYPE_DEALER)
-            endpoint = endpoints[i]
-            camera.connect(endpoint)
+            # For each desired camera, create OrcaCamera with name and endpoint
+            camera = OrcaCamera(name=names[i], endpoint=endpoints[i])
+
+            # Store in list of cameras and put tree in list of trees
             self.cameras.append(camera)
+            camtrees.append(camera.tree)
 
-            # Each camera has its own part of the tree kept in an array
-            camera_subtree = {}
-            camera_subtree["camera_name"] = names[i]
-            camera_subtree["endpoint"] = endpoint
-            camera_subtree["command"] = (lambda: None, partial(self.send_command, camera=camera))
-            camera_subtree["config"] = self.request_config(camera)
-            camtrees.append(camera_subtree)
-
-        tree['cameras'] = camtrees  # Array of cameras here
-
+        # Array of camera trees becomes real parameter tree
+        tree['cameras'] = camtrees
         self.param_tree = ParameterTree(tree)
 
-        logging.debug("instance_count: %s", num_cameras)
+        logging.debug("number of cams: %s", num_cameras)
         logging.debug("cameras: %s", self.cameras)
-
-
-    def next_msg_id(self):
-        """Return the next message id."""
-        self.msg_id += 1
-        return self.msg_id
-
-    def request_config(self, camera):
-
-        config_msg = IpcMessage('cmd', 'request_configuration', id=self.next_msg_id())
-        logging.info(f"Sending config request to {camera.identity}")
-        camera.send(config_msg.encode())
-        reply = self.await_response(camera)
-        if reply:
-            return reply.attrs['params']['camera']
-        else:
-            return False
-
-    def send_command(self, camera, value):
-        """Compose a command message to be sent to a given camera."""
-
-        self.command = {}
-        self.command["command"] = value
-
-        self.command_msg = {
-            "params": self.command
-        }
-        self.send_config_message(camera, self.command_msg)
-    
-    def send_config_message(self, camera, config):
-        """Send a configuration message to a given camera."""
-
-        msg = IpcMessage('cmd', 'configure', id=self.next_msg_id())
-        msg.attrs.update(config)
-        # logging
-        logging.debug(f"Sending configuration: {config} to {camera.identity}")
-
-        camera.send(msg.encode())
-        if not self.await_response(camera):
-            # await response from camera
-            return False
-        return True
-
-
-    def send_config_message_all(self, config):
-        """Send a configuration message to all cameras."""
-
-        all_responses_valid = True
-
-        for camera in self.cameras:
-            msg = IpcMessage('cmd', 'configure', id=self.next_msg_id())
-            msg.attrs.update(config)
-            # logging
-            logging.debug(f"Sending configuration: {config} to {camera.identity}")
-
-            camera.send(msg.encode())
-            if not self.await_response(camera):
-                # await response from camera
-                all_responses_valid = False
-
-        return all_responses_valid
-
-    def await_response(self, camera, timeout_ms=5000):
-        """Await a response on the given camera."""
-
-        pollevents = camera.poll(timeout_ms)
-
-        if pollevents == IpcChannel.POLLIN:
-            reply = IpcMessage(from_str=camera.recv())
-
-            logging.debug(f"Got response from {camera.identity}: {reply}")
-
-            return reply
-        else:
-            logging.debug(f"No response received or error occurred from {camera.identity}.")
-
-            return False
 
     def get(self, path):
         """Get the parameter tree.
---
+++ control/src/orca_quest/orca_camera.py
@@ -0,0 +1,131 @@
+from odin_data.ipc_channel import IpcChannel
+from odin_data.ipc_message import IpcMessage
+
+from functools import partial
+import logging
+
+class OrcaCamera():
+
+    def __init__(self, endpoint, name):
+
+        self.endpoint = endpoint
+        self.name = name
+        self.msg_id = 0
+
+        self.config = {}
+        self.tree = {}
+
+        self.timeout_ms = 5000
+
+        self.camera = IpcChannel(IpcChannel.CHANNEL_TYPE_DEALER)
+        self.camera.connect(endpoint)
+
+        self.tree['camera_name'] = self.name
+        self.tree['endpoint'] = self.endpoint
+        self.tree['command'] = (lambda: None, self.send_command)
+
+        initial_config = self.request_config()
+        if initial_config:
+            self.config = initial_config
+
+            self.tree['config'] = {}
+
+            for key, item in initial_config.items():
+                self.tree['config'][key] = (lambda key=key:
+                    self.config[key], partial(self.set_config, param=key)
+                )  # Function uses key (the parameter) as argument via partial
+
+        self.tree['status'] = (lambda: self.request_status(), None)
+
+    def send_command(self, value):
+        """Compose a command message to be sent to the camera.
+        :param value: command to be put into the message, PUT from tree
+        """
+        logging.debug("command: %s", value)
+        self.command_msg = {
+            "params": {
+                "command": value
+            }
+        }
+        self.send_config_message(self.command_msg)
+
+    def set_config(self, value, param):
+        """Update local storage of config values and send a command to the camera to update it.
+        :param value: argument passed by PUT request to param tree
+        :param param: config parameter to update, e.g.: exposure_time
+        """
+        self.config[param] = value
+
+        self.command_msg = {
+            "params": {
+                "camera": {
+                    param : value
+                }
+            }
+        }
+        self.send_config_message(self.command_msg)
+
+    def send_config_message(self, config):
+        """Send a configuration message to a given camera.
+        :param config: command message dictionary in {params{camera{param:value}}} structure
+        """
+        msg = IpcMessage('cmd', 'configure', id=self._next_msg_id())
+        msg.attrs.update(config)
+
+        logging.info(f"Sending configuration: {config} to {self.camera.identity}.")
+
+        self.camera.send(msg.encode())
+
+        if not self.await_response():
+            return False
+        self.request_status()  # Update status
+        return True
+
+    def request_config(self):
+        """Request configuration values from the camera.
+        :return: False, or unnested dict of attributes.
+        """
+        config_msg = IpcMessage('cmd', 'request_configuration', id=self._next_msg_id())
+        logging.info(f"Sending config request to {self.camera.identity}.")
+        self.camera.send(config_msg.encode())
+        reply = self.await_response()
+        if reply:
+            self.request_status()  # Update status
+            return reply.attrs['params']['camera']
+
+    def request_status(self):
+        """Get and return the current status of a connected camera.
+        :return status: dictionary of current camera status"""
+        status_msg = IpcMessage('cmd', 'status', id=self._next_msg_id())
+        self.camera.send(status_msg.encode())
+
+        response = self.await_response(silence_reply=True)
+        if response:
+            return response.attrs['params']['status']
+        else:
+            return False
+
+    def await_response(self, timeout_ms=5000, silence_reply=False):
+        """Await a response on the given camera.
+        :param timeout_ms: timeout in milliseconds
+        :return: response, or False
+        """
+        pollevents = self.camera.poll(timeout_ms)
+
+        if pollevents == IpcChannel.POLLIN:
+            reply = IpcMessage(from_str=self.camera.recv())
+
+            if not silence_reply:
+                logging.info(f"Got response from {self.camera.identity}: {reply}")
+
+            return reply
+        else:
+            logging.debug(f"No response received, or error occurred, from {self.camera.identity}")
+
+            return False
+
+    def _next_msg_id(self):
+        """Return the next (incremented) message id."""
+
+        self.msg_id += 1
+        return self.msg_id
